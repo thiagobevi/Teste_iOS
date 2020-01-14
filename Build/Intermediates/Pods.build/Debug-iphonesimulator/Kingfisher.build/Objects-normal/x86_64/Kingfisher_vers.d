f7f0066c856c04ac94cd52158a3237f123ef5a34
Thiago Bevilacqua <thiagobevi@hotmail.com>
2020-01-14 14:45:42 -0300

    Teste

---
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Kingfisher.build/Objects-normal/x86_64/Kingfisher_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ThiagoBevi/Downloads/Teste_iOS-TesteiOS_BASE_API/TestTMBD/Build/Intermediates/Pods.build/Debug-iphonesimulator/Kingfisher.build/DerivedSources/Kingfisher_vers.c
---
+++ Pods/Target Support Files/Pods-TestTMBDTests/Pods-TestTMBDTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestTMBDTests : NSObject
+@end
+@implementation PodsDummy_Pods_TestTMBDTests
+@end
